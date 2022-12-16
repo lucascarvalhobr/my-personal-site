133f0f3ad2bba6703ec5ac5ecd309357725de2c8
Lucas Carvalho <lucascarvalho34@gmail.com>
2022-12-16 15:27:47 -0300

    feat: uploading basic content

---
+++ src/web-app/js/navbaranimationfix.js
@@ -0,0 +1,8 @@
+                         
+$('#menu-responsivo').on('show.bs.collapse', function () {
+  $('.getCodeBanner').css('transform', 'translate(-50%,30%)');}
+);
+
+$('#menu-responsivo').on('hide.bs.collapse', function () {
+  $('.getCodeBanner').css('transform', 'translate(-50%,-50%)');}
+);
